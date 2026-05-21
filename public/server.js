7c8f808e19ab4ee4253ebcffb4bff533cbe434fa
vhhcfghvb124-spec <vhhcfghvb124@gmail.com>
2026-05-21 19:23:32 +0300

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,1 +1,15 @@
-bbhhjkj
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'عام')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'عام', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
